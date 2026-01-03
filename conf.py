f4c539880619e51a669240c8f93143ef0689990e
ralphzinsky <ralphzinsky@proton.me>
2026-01-03 18:58:16 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "to Log In to Dashlane: Step-by-Step Guide to Access and Manage Your Account Securely"
+html_title = "CCleaner Professional Plus Setup Guide: Install on Windows 11 or 10, Activate with Download Key, and Transfer to a New PC"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
